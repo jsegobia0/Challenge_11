b75fb02067626c2409bbc7108e7a81c65e7b64ae
jsegobia0 <jsegobia0@yahoo.com>
2021-08-28 15:19:28 -0700

    updated files

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
 // GIVEN a command-line application that accepts user input
 
 // WHEN I am prompted for information about my application repository
