bcc3312e1ba525abfcbdaa68b8275c809e4c7c99
jsegobia0 <jsegobia0@yahoo.com>
2021-08-28 16:36:00 -0700

    updated file

--- index.js
+++ index.js
@@ -53,6 +53,15 @@ const questions = [
     message: "GitHub username?"
 },
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+// WHEN I enter my email address
+{
+    type: "input",
+    name: "questions",
+    message: "What is your email address?"
+},
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
 ];
 
 // function to write README file
@@ -62,7 +71,9 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+// WHEN I am prompted for information about my application repository
     inquirer.prompt(questions)
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
     .then((inquirerResponses) => {
         console.log("Generating README...");
         writeToFile("READEME.md", generateMarkdown({...inquirerResponses}));
@@ -74,28 +85,6 @@ function init() {
 init();
 
 // GIVEN a command-line application that accepts user input
-
-// WHEN I am prompted for information about my application repository
-
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-
-
-
-
-
-
-// WHEN I choose a license for my application from a list of options
-
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-
-
-
-
-// WHEN I enter my email address
-
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 
 // WHEN I click on the links in the Table of Contents
 
