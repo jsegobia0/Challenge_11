f4686e11f7bbb07fff4f8c738ad5cb814ba09bfa
jsegobia0 <jsegobia0@yahoo.com>
2021-08-28 21:11:43 -0700

    updated file

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-const { promisify } = require("util");
 
 // array of questions for user
 const questions = [
@@ -68,7 +67,7 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
-   const tdb = fs.writeToFileSync(path, filename, data);
+     // tdb fs.writeToFileSync(..., filename, data);
 }
 
 // function to initialize program
