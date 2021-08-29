4695487c9f2de0da6acecf4c7e3ee4e68d50d78a
jsegobia0 <jsegobia0@yahoo.com>
2021-08-28 18:08:08 -0700

    updated file

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "Title of README?"
+        message: "What is the title of your README?"
     },
 // THEN this is displayed as the title of the README
 
